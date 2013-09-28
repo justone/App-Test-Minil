1ef4b69bc44d18aa79b37bd40c72142949e4238e
Nate Jones <nate@endot.org>
2013-09-28 12:04:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Test::Minil - It's new $module
+
+# SYNOPSIS
+
+    use App::Test::Minil;
+
+# DESCRIPTION
+
+App::Test::Minil is ...
+
+# LICENSE
+
+Copyright (C) Nate Jones.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Nate Jones <nate@endot.org>
---
+++ lib/App/Test/Minil.pm
@@ -0,0 +1,39 @@
+package App::Test::Minil;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Test::Minil - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Test::Minil;
+
+=head1 DESCRIPTION
+
+App::Test::Minil is ...
+
+=head1 LICENSE
+
+Copyright (C) Nate Jones.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Nate Jones E<lt>nate@endot.orgE<gt>
+
+=cut
+
